4980eb000e8bcb931a20d14d252de4ce51b3b570
James-Cochran <jameslikesfruit@gmail.com>
2025-01-11 12:40:43 -0500

    expanded homepage component to include app title, messaging, and begin button.

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+  <div>
+    <header>  
+      <h1> Journey Like a Genius </h1>
+      <p>
+        We're excited to get started on your personalized travel itinerary
+      </p>
+      <p>
+        We'll ask a series of questions to better understand you lifestyle,
+        preferences, and unique needs so we can generate a perfect travel day
+      </p>
+      <button> Let's get started </button>
+    </header>
+    </div>
+  );
 };
 
 export default Homepage;
