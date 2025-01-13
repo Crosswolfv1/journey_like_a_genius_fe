11e8a07527c3aa4ce4f31f5a7e8fb7fd2b6a1879
Jeremiah Ross <crosswolf.20work@gmail.com>
2025-01-13 11:55:45 -0600

    renamed map compnenet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import Map from "./components/Map";
+import GoogleMapsComponent from "./components/GoogleMapsComponent";
 
 const App = () => {
   return (
+    
     <div style={{ maxWidth: "800px", margin: "0 auto", padding: "20px" }}>
       <h1>Google Maps with React</h1>
-      <Map />
+      <GoogleMapsComponent />
     </div>
   );
 };
