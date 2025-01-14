9dec0cd02ebd956887e40c947d464581ace2438d
Alora Riley <alora.riley@gmail.com>
2025-01-14 18:05:10 -0500

    feat: Implement Router.

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import React, { useState } from "react";
 import Map from "./components/Map";
 import Homepage from "./components/Homepage";
 import Preferences from "./components/Preferences";
+import Itinerary from "./components/Itinerary";
+import { Routes, Route} from 'react-router-dom';
 
 const App = () => {
   const [showPreferences, setShowPreferences] = useState(false)
 
   return (
     <div style={{ maxWidth: "800px", margin: "0 auto", padding: "20px" }}>
-      {/* <h1>Google Maps with React</h1>
-      <Map /> */}
-      { !showPreferences ? (
-        <Homepage setShowPreferences={setShowPreferences} /> 
-        ) : (
-          <Preferences />
-        ) }
+      <Routes>
+        <Route path="/" element={
+          !showPreferences ? (
+            <Homepage setShowPreferences={setShowPreferences} /> ) : (<Preferences />) } />
+          // {/* <h1>Google Maps with React</h1>
+          // <Map /> */}
+        <Route path="/itinerary/:user_id" element={<Itinerary/>} />
+      </Routes>
     </div>
   );
 };
---
+++ src/components/Itinerary.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Itinerary.css";
+
+const Itinerary = () => {
+
+    return (
+      <main className="itinerary-container">
+        <h1>Journey Like a Genius</h1>
+        <section>
+          <p>Itinerary</p>
+        </section>
+      </main>
+    )
+}
+
+export default Itinerary;
