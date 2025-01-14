f475605a0be0a68ea867f37994ac420ce138f7b0
Jeremiah Ross <crosswolf.20work@gmail.com>
2025-01-14 09:57:52 -0600

    Feat: Added search bar, modified buttons to search what term is in the earch bar

--- src/components/GoogleMapsComponent.js
+++ src/components/GoogleMapsComponent.js
@@ -6,12 +6,13 @@ const GoogleMapsComponent = () => {
   const { isLoaded, error } = useGoogleMaps(apiKey); // Proper destructuring
   const mapRef = useRef(null);
   const [places, setPlaces] = useState([]);
+  const [searchTerm, setSearch] = useState('')
 
-  async function findPlaces(query) {
+  async function findPlaces() {
     const { Place } = await window.google.maps.importLibrary("places");
-    const { AdvancedMarkerElement } = await window.google.maps.importLibrary("marker");
+    // const { AdvancedMarkerElement } = await window.google.maps.importLibrary("marker");
     const request = {
-      textQuery: query,
+      textQuery: `${searchTerm} in Tokyo`,
       fields: ["displayName", "id", "accessibilityOptions", "allowsDogs", "formattedAddress", "isGoodForChildren", "isGoodForGroups", "priceLevel", "types"],
     };
     const { places } = await Place.searchByText(request);
@@ -33,6 +34,11 @@ const GoogleMapsComponent = () => {
     }
   }, [isLoaded]); // Add isLoaded as a dependency
 
+  useEffect(() => {
+    const userInput = searchTerm
+    setSearch(userInput);
+  }, [searchTerm]);
+
   if (error) {
     return <p>Error: {error}</p>; // Render the error message
   }
@@ -41,12 +47,20 @@ return (
     <div>
       {isLoaded ? (
         <div>
+          <input 
+            aria-label="Title Search Bar"
+            type='text'
+            value={searchTerm}
+            onChange={(event) => {
+              setSearch(event.target.value)}}
+            placeholder={`Search`} />
+
           <div ref={mapRef} style={{ width: "100%", height: "400px" }}></div>
 
           <div>
-            <button onClick={() => findPlaces("musuems in tokyo")}>Search musuems in tokyo</button>
-            <button onClick={() => findPlaces("parks in tokyo")}>Search parks in tokyo</button>
-            <button onClick={() => findPlaces("ramen in tokyo")}>Search ramen in tokyo</button>
+            <button onClick={() => findPlaces(`search in Tokyo`)}>Search in tokyo</button>
+            {/* <button onClick={() => findPlaces("parks in tokyo")}>Search parks in tokyo</button>
+            <button onClick={() => findPlaces("ramen in tokyo")}>Search ramen in tokyo</button> */}
           </div>
 
           <div>
@@ -56,6 +70,7 @@ return (
                 <li key={index}>
                   <p><strong>{place.displayName}</strong></p>
                   <p>{place.adrFormatAddress}</p>
+                  <p>Price Level: {place.priceLevel ? place.priceLevel : "Not Specified"}</p>
                   <p>Accessibility: </p>
                   <ul>
                     <li>
