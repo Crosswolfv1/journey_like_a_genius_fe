52bdbad19a5b786e695c4a02672d4e8efadd4334
Alora Riley <alora.riley@gmail.com>
2025-01-15 00:06:24 -0500

    Add thank you message and Try another itinerary button with styling.

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Homepage.css";
 import { Link } from 'react-router-dom'
 
-const Homepage = ({ setShowPreferences }) => {
+const Homepage = () => {
   return (
   <main>
     <header>  
--- src/components/Itinerary.js
+++ src/components/Itinerary.js
@@ -8,10 +8,12 @@ const Itinerary = () => {
         <h1 className="title">Journey Like a Genius</h1>
           <section className="itinerary-content">
             <div className="itinerary-details">
+            <h4 className="itinerary-generated-message">Thank you for your information. A personalized itinerary has been generated to meet your specific needs.</h4>
               <p>Itinerary Item 1</p>
               <p>Itinerary Item 2</p>
               <p>Itinerary Item 3</p>
               <p>Itinerary Item 4</p>
+              <button>Try another itinerary</button>
             </div>
           </section>
       </main>
