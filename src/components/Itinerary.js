5441745340a973871387853e7b5cfe32b9b84b8a
Jeremiah Ross <crosswolf.20work@gmail.com>
2025-01-15 12:24:23 -0600

    Feat: Imported useLocation, useState
    transferred api call from preferences to itinerary
    
    added api call for food places
    
    fixed minor formatting issues

--- src/components/Itinerary.js
+++ src/components/Itinerary.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router";
 import "./Itinerary.css";
 
 const Itinerary = () => {
+  const [activityPlaces, setActivityPlaces] = useState([]);  
+  const [foodPlaces, setFoodPlaces] = useState([]);  
 
   const dummyItems = [
     {id: 1, item_type: "restaurant", name: "Café de Flore", address: "172 Bd Saint-Germain, 75006 Paris, France", opening_hours: [
@@ -18,26 +21,68 @@ const Itinerary = () => {
       "Not specified",
      ], phone: "Not specified"},
   ]
+  const location = useLocation()
+  const preferences = location.state
 
-    return (
-      <main className="itinerary-container">
-        <h1 className="title">Journey Like a Genius</h1>
-          <section className="itinerary-content">
-            <div className="itinerary-details">
-            <h4 className="itinerary-generated-message">Thank you for your information. A personalized itinerary has been generated to meet your needs.</h4>
-                <p>
-                  {dummyItems.map((item) => (
-                    <p><strong>{item.name}</strong><br /> 
-                      {item.address}<br /> 
-                      Regular Hours: {item.opening_hours}<br /> 
-                      {item.phone}</p>
-                  ))}
-                </p>
-              <button>Try another itinerary</button>
-            </div>
-          </section>
-      </main>
-    )
+  function handleSubmit() {
+    findActivityPlaces(preferences)
+    findFoodPlaces(preferences)
+    console.log('Preferences to pass to API:', preferences)
+  }
+
+  async function findActivityPlaces() {
+    const { Place } = await window.google.maps.importLibrary("places");
+    const activityRequest = {
+      textQuery: `${preferences.activityType} in ${preferences.searchTerm}`,
+      fields: ["displayName", "id", "accessibilityOptions", "allowsDogs", "formattedAddress", "isGoodForChildren", "isGoodForGroups", "priceLevel", "types", "regularOpeningHours", "internationalPhoneNumber"],
+    };
+    const { places } = await Place.searchByText(activityRequest);
+    setActivityPlaces(places)
+  }  
+
+  async function findFoodPlaces() {
+    const { Place } = await window.google.maps.importLibrary("places");
+    const foodRequest = {
+      textQuery: `${preferences.foodType} food in ${preferences.searchTerm}`,
+      fields: ["displayName", "id", "accessibilityOptions", "allowsDogs", "formattedAddress", "isGoodForChildren", "isGoodForGroups", "priceLevel", "types", "regularOpeningHours", "internationalPhoneNumber"],
+    };
+    const { places } = await Place.searchByText(foodRequest);
+    setFoodPlaces(places)
+  }  
+
+  useEffect(() => {
+    activityPlaces.forEach(element => {
+      console.log("Updated activityPlaces:", element.Eg);
+    })
+    foodPlaces.forEach(element => {
+      console.log("food places:", element.Eg)
+    })
+  }, [activityPlaces, foodPlaces]);
+
+  useEffect(() => {
+    console.log(preferences)
+    handleSubmit(preferences)
+  }, [])
+
+  return (
+    <main className="itinerary-container">
+      <h1 className="title">Journey Like a Genius</h1>
+        <section className="itinerary-content">
+          <div className="itinerary-details">
+          <h4 className="itinerary-generated-message">Thank you for your information. A personalized itinerary has been generated to meet your needs.</h4>
+              <p>
+                {dummyItems.map((item) => (
+                  <p><strong>{item.name}</strong><br /> 
+                    {item.address}<br /> 
+                    Regular Hours: {item.opening_hours}<br /> 
+                    {item.phone}</p>
+                ))}
+              </p>
+            <button>Try another itinerary</button>
+          </div>
+        </section>
+    </main>
+  )
 }
 
 export default Itinerary;
