ceb680df42e5c6bf5b8937e55661be8dd8c0697e
Alora Riley <alora.riley@gmail.com>
2025-01-17 18:51:17 -0500

    teat: Add fixture to spec file.

--- cypress/e2e/itinerary_spec.cy.js
+++ cypress/e2e/itinerary_spec.cy.js
@@ -2,7 +2,8 @@ describe('itinerary spec', () => {
   beforeEach(() => {
     cy.intercept("GET", "http://localhost:3001/api/v1/itinerary", {
       statusCode: 200,
-      fixture: "preferences"
+      fixture: "preferences",
+      fixture: "itinerary_item"
     })
     cy.visit('http://localhost:3000')
   })
