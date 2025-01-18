c1619c4788b193e984f02304fe8f26c78f684b4e
Jeremiah Ross <crosswolf.20work@gmail.com>
2025-01-18 13:08:09 -0600

    fixed URl directer

--- src/components/Itinerary.js
+++ src/components/Itinerary.js
@@ -244,7 +244,7 @@ const Itinerary = () => {
             <div className="btn-container">
             <button className="try-again-button" onClick={handleTryAgain}>Try another itinerary</button>
               <button className="save-button" onClick={saveItinerary}>Save itinerary</button>
-              <Link to={`/${userId}`}>
+              <Link to={`/${userId.userId}`}>
                 <button className="return-home">Back to home</button>
               </Link>
             </div>
