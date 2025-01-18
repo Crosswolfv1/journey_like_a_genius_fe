adbdc942af259b3477cfc36624e926aadef85ad5
Alora Riley <alora.riley@gmail.com>
2025-01-18 12:07:58 -0500

    test: Make assertions for cypress tests.

--- cypress/e2e/itinerary_spec.cy.js
+++ cypress/e2e/itinerary_spec.cy.js
@@ -1,17 +1,28 @@
-describe('itinerary spec', () => {
+describe('Itinerary Component', () => {
   beforeEach(() => {
-    cy.intercept("GET", "http://localhost:3001/api/v1/itinerary", {
-      statusCode: 200,
-      fixture: "preferences",
-      fixture: "itinerary_item_1",
-      fixture: "itinerary_item_2", 
-      fixture: "itinerary_item_3",
-      fixture: "itinerary_item_4"
+    cy.fixture('preferences').then((preferences) => {
+      cy.log('Preferences:', JSON.stringify(preferences)); 
+      cy.visit('http://localhost:3000/itinerary/1', {
+        state: preferences,
+      })
     })
-    cy.visit('http://localhost:3000')
   })
 
-  it('displays title on page load', () => {
-    cy.get('h1').contains('Journey Like a Genius')
+  it('displays the title', () => {
+    cy.get('h1').should('contain', 'Journey Like a Genius')
+  })
+
+  it('displays the personalized itinerary message', () => {
+    cy.get('h4.itinerary-generated-message').should('contain', 'Thank you for your information. A personalized itinerary has been generated to meet your needs.')
+  })
+
+  it('displays itinerary details', () => {
+    cy.get('.itinerary-details p').should('exist')
+  })
+
+  it('has functional buttons', () => {
+    cy.get('button.try-again-button').should('exist')
+    cy.get('button.save-button').should('exist')
+    cy.get('button.return-home').should('exist')
   })
 })
