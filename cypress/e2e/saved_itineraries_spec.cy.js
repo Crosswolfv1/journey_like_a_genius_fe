7e95d046127818b02be4be90f679895afd7ef0a2
Jeremiah Ross <crosswolf.20work@gmail.com>
2026-03-01 11:48:57 -0600

    Updated tests to accurately check DOM elements

--- cypress/e2e/saved_itineraries_spec.cy.js
+++ cypress/e2e/saved_itineraries_spec.cy.js
@@ -1,12 +1,10 @@
 describe('Saved Itineraries User Flows', () => {
   beforeEach(() => {
-    cy.viewport(11440, 900)
-    cy.visit('https://journeylikeagenius.netlify.app/')
+    cy.visit('http://localhost:3000')
+    cy.contains('View Saved Itineraries').click()
     cy.intercept('GET', '**/api/v1/itineraries', {
       fixture: 'saved_itineraries.json',
-    }).as('getItineraries')
-    cy.contains('View Saved Itineraries').click()
-    cy.wait('@getItineraries')
+    })
   })
 
   it('should display the correct title and "Back to Home" button', () => {
@@ -17,7 +15,7 @@ describe('Saved Itineraries User Flows', () => {
   it('should direct a user to the homepage when "Back to Home" button is clicked', () => {
     cy.get('button').contains('Back to home').should('exist')
     cy.get('button').contains('Back to home').click()
-    cy.url().should('eq', 'https://journeylikeagenius.netlify.app/')
+    cy.url().should('eq', 'http://localhost:3000/')
   })
 
   it('should display all saved itineraries', () => {
@@ -29,12 +27,13 @@ describe('Saved Itineraries User Flows', () => {
   
   it('should render all itinerary details correctly', () => {
     cy.get('.saved-single-itinerary').eq(0).within(() => {
-      cy.get('h4').should('contain', 'Itinerary for Paris - half day')
-      cy.get('.saved-itinerary-items').should('contain', 'No items for this itinerary.')
+    cy.get('h3.saved-itinerary-title')
+      .should('contain', ' Itinerary for ')
+    cy.get('.saved-itinerary-items').should('contain', 'No items for this itinerary.')
     })
   
     cy.get('.saved-single-itinerary').eq(1).within(() => {
-      cy.get('h4').should('contain', 'Itinerary for San Antonio - half-day')
+      cy.get('h3.saved-itinerary-title').should('contain', 'Itinerary for San Antonio - half-day')
       cy.get('.saved-itinerary-items').within(() => {
         cy.get('strong').eq(0).should('contain', "Pinkerton's Barbecue")
         cy.get('strong').eq(1).should('contain', 'Headwaters Sanctuary')
@@ -42,7 +41,7 @@ describe('Saved Itineraries User Flows', () => {
     })
   
     cy.get('.saved-single-itinerary').eq(2).within(() => {
-      cy.get('h4').should('contain', 'Itinerary for San Antonio - half-day')
+      cy.get('h3').should('contain', 'Itinerary for San Antonio - half-day')
       cy.get('.saved-itinerary-items').within(() => {
         cy.get('strong').eq(0).should('contain', 'El Monte BBQ')
         cy.get('strong').eq(1).should('contain', 'Natural Bridge Caverns')
@@ -54,7 +53,7 @@ describe('Saved Itineraries User Flows', () => {
     cy.intercept('GET', '**/api/v1/itineraries', {
       fixture: 'no_itineraries.json',
     }).as('getNoItineraries')
-    cy.visit('https://journeylikeagenius.netlify.app/')
+    cy.visit('http://localhost:3000')
     cy.contains('View Saved Itineraries').click()
     cy.wait('@getNoItineraries')
     cy.get('.saved-single-itinerary').should('not.exist')
